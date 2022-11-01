44c5dc8d4609454d27201f6ae673b9ca4ab16db5
haswanth <v.haswanthreddy@gmail.com>
2022-11-02 01:15:44 +0530

    implemented solution in classes

--- geektrust.js
+++ geektrust.js
@@ -1,393 +1,38 @@
-const fs = require("fs")
+/* eslint-disable no-restricted-syntax */
+const fs = require('fs');
 const {
-    TRACK_TYPE,
-    BOOKING_START_TIME,
-    BOOKING_END_TIME,
-    MAX_VEHICLES_PER_TRACK,
-    TYPE_OF_VEHICLE,
-    BOOKING_TYPE,
-    ADDITIONAL_END_TIME,
-    COST_PER_HOUR,
-} = require('./constants')
+	BOOK,
+	REVENUE,
+	ADDITIONAL,
+} = require('./constants');
+const RaceTrack = require('./racetrack');
 
-const filename = process.argv[2]
+const handleInput = (data) => {
+	const racetrackSession = new RaceTrack();
+	for (const input of data) {
+		const commands = input.split(' ');
 
-class RaceTrack {
-    constructor() {
-        this.bookingData = [];
-    }
-    // add # in front of private methods
+		if (commands[0] === BOOK) {
+			const [vehicleType, vehicleNo, entryTime] = commands.slice(1, 4);
 
-    getTotalHours = (timeString1, timeString2) => {
-        const [hours1, mins1] = timeString1.split(':');
-        const [hours2, mins2] = timeString2.split(':');
+			racetrackSession.book(vehicleType, vehicleNo, entryTime);
+		} else if (commands[0] === ADDITIONAL) {
+			const [vehicleNo, exitTime] = commands.slice(1, 3);
 
-        const diff = (Number(hours1) * 60 + Number(mins1)) - (Number(hours2) * 60 + Number(mins2))
+			racetrackSession.additional(vehicleNo, exitTime);
+		} else if (commands[0] === REVENUE) {
+			racetrackSession.revenue();
+		}
+	}
+};
 
-        const totalHours = Math.floor(diff / 60) + (((diff / 60) - Math.floor(diff / 60)) > 0 ? 1 : 0)
+// readFile
+const data = fs.readFileSync(process.argv[2], 'utf-8').toString().replace(/\r|/g, '').split('\n');
 
-        return totalHours
-    }
-
-    addTime = (timeString, timeToAddInMinutes) => {
-        const [hour, minutes] = timeString.split(':');
-        const date = new Date();
-        date.setHours(hour, minutes, 0);
-        date = new Date(date.getTime() + timeToAddInMinutes * 1000)
-        date.toString();
-        return `${date.getHours()}:${date.getMinutes()}`
-    }
-
-    vacancyCheck = (vehicleType, entryTime, exitTime) => {
-        let countOfVehiclesVip = 0;
-        let countOfVehiclesRegular = 0;
-
-        if (bookingData.length >= 1) {
-            bookingData.forEach((booking) => {
-                if (booking.vehicleType !== vehicleType) {
-                    return
-                }
-
-                if (!(
-                    ((booking.entryTime < exitTime) && (booking.exitTime >= exitTime))
-                    || ((booking.entryTime <= entryTime) && (booking.exitTime > entryTime))
-                )) {
-                    return
-                }
-
-                if (booking.trackType === TRACK_TYPE.REGULAR) {
-                    countOfVehiclesRegular += 1
-                } else {
-                    countOfVehiclesVip += 1
-                }
-            })
-
-            return {
-                regular: MAX_VEHICLES_PER_TRACK.REGULAR[vehicleType] - countOfVehiclesRegular,
-                vip: MAX_VEHICLES_PER_TRACK.VIP[vehicleType] - countOfVehiclesVip
-            }
-        }
-
-        return {
-            regular: MAX_VEHICLES_PER_TRACK.REGULAR[vehicleType],
-            vip: MAX_VEHICLES_PER_TRACK.VIP[vehicleType]
-        }
-    }
-
-    book = (vehicleType, vehicleNo, entryTime) => {
-        const exitTime = this.addTime(entryTime, 3 * 60)
-
-        const trackVacancy = this.vacancyCheck(vehicleType, entryTime, exitTime)
-
-        if (trackVacancy.regular < 1 && ((trackVacancy.vip < 1) || (vehicleType === 'BIKE'))) {
-            return 'RACE_TRACK_FULL'
-        }
-
-        const booking = {
-            vehicleType,
-            entryTime,
-            vehicleNo,
-            trackType: trackVacancy.regular >= 1 ? TRACK_TYPE.REGULAR : TRACK_TYPE.VIP,
-            bookingType: BOOKING_TYPE.BOOKING,
-            exitTime: exitTime,
-        }
-
-        this.bookingData.push(booking)
-
-        return 'SUCCESS'
-    }
-
-    additional = (vehicleNo, newExitTime) => {
-        let vehicleData;
-
-        for (let booking of this.bookingData) {
-            if (booking.vehicleNo === vehicleNo && booking.bookingType === BOOKING_TYPE.BOOKING)
-                vehicleData = booking
-        }
-
-        if (!vehicleData) {
-            return 'NO_VEHICLE_EXISTS'
-        }
-
-        const {
-            vehicleType,
-            exitTime,
-        } = vehicleData
-
-        const trackVacancy = this.vacancyCheck(vehicleType, exitTime, newExitTime)
-
-        if (trackVacancy.regular < 1 && ((trackVacancy.vip < 1) || (vehicleType === 'BIKE'))) {
-            return 'RACE_TRACK_FULL'
-        }
-
-        const bookingAddition = {
-            entryTime: exitTime,
-            exitTime: newExitTime,
-            vehicleType: vehicleType,
-            vehicleNo,
-            trackType: trackVacancy.regular >= 1 ? TRACK_TYPE.REGULAR : TRACK_TYPE.VIP,
-            bookingType: BOOKING_TYPE.ADDITIONAL,
-        }
-
-        this.bookingData.push(bookingAddition);
-
-        return 'SUCCESS'
-    }
-
-    revenue = () => {
-        let vipTrackIncome = 0;
-        let regularTrackIncome = 0;
-
-        this.bookingData.forEach((booking) => {
-
-            // booking
-            if (booking.bookingType === BOOKING_TYPE.BOOKING) {
-                if (booking.trackType === TRACK_TYPE.VIP) {
-                    vipTrackIncome += (COST_PER_HOUR.VIP[booking.vehicleType] * 3)
-                }
-                else {
-                    regularTrackIncome += (COST_PER_HOUR.REGULAR[booking.vehicleType] * 3)
-                }
-                return
-            }
-
-            // additional
-            if (this.addTime(booking.entryTime, 15) > booking.exitTime) return
-
-            const totalHours = this.getTotalHours(booking.entryTime, booking.exitTime)
-
-            if (booking.trackType === TRACK_TYPE.VIP) {
-                vipTrackIncome += (50 * totalHours)
-            }
-            else {
-                regularTrackIncome += (50 * totalHours)
-            }
-        })
-
-        return [regular, vip]
-    }
-}
-
-const getHours = (timeString1, timeString2) => {
-    const [hrs1, mins1] = timeString1.split(':');
-    const [hrs2, mins2] = timeString2.split(':');
-
-    const diff = Number(hrs1) * 60 + Number(mins1) - Number(hrs2) * 60 + Number(mins2)
-    let [hours, mins] = (diff / 60).toString().split('.')
-    hours = Number(hours)
-    hours += mins > '0' ? 1 : 0
-    return hours
-}
-
-const addTime = (timeString, timeToAdd) => {
-    function convertToString(remainder) {
-        return (remainder < 10 ? '0' : '') + remainder;
-    }
-
-    const [hour, minutes] = timeString.split(':');
-    let mins = (+hour) * 60 + +minutes + +timeToAdd
-
-    return (convertToString(mins % (24 * 60) / 60 | 0) + ':' + convertToString(mins % 60))
-}
+handleInput(data);
 
 /**
- * @desc To check vacancy in track according to vehicle type and entry and exit time
- * @param {*} vehicleType 
- * @param {*} entryTime 
- * @param {*} exitTime 
- * @returns no. of vehicles in vip and regular
- */
-
-const vacancyCheck = (vehicleType, entryTime, exitTime) => {
-    let countOfVehiclesVip = 0;
-    let countOfVehiclesRegular = 0;
-
-    if (booking_data.length >= 1) {
-        booking_data.forEach((booking) => {
-            if (booking.vehicleType !== vehicleType) {
-                return
-            }
-
-            let inBookingTime = ((booking.entryTime < exitTime) && (booking.exitTime > exitTime))
-                || ((booking.entryTime < entryTime) && (booking.exitTime > entryTime))
-                || ((booking.entryTime === entryTime) || (booking.exitTime === exitTime))
-
-            if (!inBookingTime) {
-                return
-            }
-
-            if (booking.trackType === TRACK_TYPE.REGULAR) {
-                countOfVehiclesRegular += 1
-            } else {
-                countOfVehiclesVip += 1
-            }
-        })
-
-        return {
-            regular: MAX_VEHICLES_PER_TRACK.REGULAR[vehicleType] - countOfVehiclesRegular,
-            vip: MAX_VEHICLES_PER_TRACK.VIP[vehicleType] - countOfVehiclesVip
-        }
-    }
-
-    return {
-        regular: MAX_VEHICLES_PER_TRACK.REGULAR[vehicleType],
-        vip: MAX_VEHICLES_PER_TRACK.VIP[vehicleType]
-    }
-}
-
-
-/**
- * @desc To book a vehicles booking
- * @param {*} vehicleType 
- * @param {*} vehicleNo 
- * @param {*} entryTime 
- * @returns success if booked else race-track-full if track is occupied
- */
-const book = (vehicleType, vehicleNo, entryTime) => {
-
-    const entryDateTime = entryTime//getDateTime(entryTime)
-    const exitTime = addTime(entryTime, 3 * 60)
-
-    const trackVacancy = vacancyCheck(vehicleType, entryDateTime, exitTime)
-
-    if (trackVacancy.regular < 1 && ((trackVacancy.vip < 1) || (vehicleType === 'BIKE'))) {
-        return 'RACE_TRACK_FULL'
-    }
-
-    const booking = {
-        vehicleType,
-        entryTime: entryDateTime,
-        trackType: null,
-        vehicleNo,
-        bookingType: BOOKING_TYPE.BOOKING,
-        exitTime: addTime(entryDateTime, 3 * 60)
-    }
-
-    booking.trackType = trackVacancy.regular >= 1 ? TRACK_TYPE.REGULAR : TRACK_TYPE.VIP
-    booking_data.push(booking)
-
-    vehicle_data[vehicleNo] = booking
-
-    return 'SUCCESS'
-}
-
-/**
- * @desc to handle additional booking for a vehicle
- * @param {*} vehicleNo 
- * @param {*} newExitTime 
- * @returns 
- */
-const additional = (vehicleNo, newExitTime) => {
-    const vehicleData = vehicle_data[vehicleNo];
-
-    if (!vehicleData) {
-        return 'NO_VEHICLE_EXISTS'
-    }
-
-    const {
-        vehicleType,
-        exitTime,
-    } = vehicleData
-
-    const timeToCheck = addTime(exitTime, 15)
-
-    if (timeToCheck > newExitTime) {
-        return
-    }
-
-    const trackVacancy = vacancyCheck(vehicleType, exitTime, newExitTime)
-
-    if (trackVacancy.regular < 1 && ((trackVacancy.vip < 1) || (vehicleType === 'BIKE'))) {
-        return 'RACE_TRACK_FULL'
-    }
-
-    const bookingAddition = {
-        entryTime: exitTime,
-        exitTime: newExitTime,
-        vehicleType: vehicleType,
-        vehicleNo,
-        trackType: trackVacancy.regular >= 1 ? TRACK_TYPE.REGULAR : TRACK_TYPE.VIP,
-        bookingType: BOOKING_TYPE.ADDITIONAL,
-    }
-
-    booking_data.push(bookingAddition);
-
-    return 'SUCCESS'
-}
-
-
-
-/**
- * @desc to handle calculation of revenue
- * @returns regular and vip tracks income
- */
-const revenue = () => {
-    vip = 0;
-    regular = 0;
-    count = 1
-    booking_data.forEach((booking) => {
-
-        // booking
-        if (booking.bookingType === BOOKING_TYPE.BOOKING) {
-            if (booking.trackType === TRACK_TYPE.VIP) vip += (COST_PER_HOUR.VIP[booking.vehicleType] * 3)
-            else regular += (COST_PER_HOUR.REGULAR[booking.vehicleType] * 3)
-
-            return
-        }
-
-        let hours = getHours(booking.entryTime, booking.exitTime)
-        if (booking.trackType === TRACK_TYPE.VIP) vip += (50 * hours)
-        else regular += (50 * hours)
-    })
-
-    return [regular, vip]
-}
-
-
-const handleInput = (inputArr) => {
-    const booking_data = []
-
-    const vehicle_data = {
-
-    }
-    if (inputArr[0] === 'BOOK') {
-
-        const vehicleType = inputArr[1];
-
-        const vehicleNo = inputArr[2];
-
-        const entryTime = inputArr[3];
-
-        if ((entryTime < BOOKING_START_TIME) || (entryTime > BOOKING_END_TIME)) {
-            console.log('INVALID_ENTRY_TIME')
-            return
-        }
-
-        if (!TYPE_OF_VEHICLE[vehicleType]) {
-            console.log('INVALID_VEHICLE_TYPE');
-            return
-        }
-
-        console.log(book(vehicleType, vehicleNo, entryTime))
-    } else if (inputArr[0] === 'ADDITIONAL') {
-        const vehicleNo = inputArr[1];
-
-        const exitTime = inputArr[2];
-
-        if ((exitTime < BOOKING_START_TIME) || (exitTime > ADDITIONAL_END_TIME)) {
-            console.log('INVALID_EXIT_TIME')
-            return
-        }
-
-        console.log(additional(vehicleNo, exitTime))
-    } else if (inputArr[0] === 'REVENUE') {
-        const [regular, vip] = revenue()
-        console.log(`${regular} ${vip}`)
-    }
-}
-
-//readFile
-const read = (filename) => fs.readFile(filename, "utf8", (err, data) => {
+ * , "utf8", (err, data) => {
     if (err) throw err
 
     const inputLines = data.toString().split("\n")
@@ -397,13 +42,4 @@ const read = (filename) => fs.readFile(filename, "utf8", (err, data) => {
         handleInput(inputArr)
     })
 })
-
-filename && read(filename)
-
-
-
-
-
-
-
-
+ */
---
+++ racetrack.js
@@ -0,0 +1,198 @@
+const {
+	TRACK_TYPE,
+	BOOKING_START_TIME,
+	BOOKING_END_TIME,
+	MAX_VEHICLES_PER_TRACK,
+	TYPE_OF_VEHICLE,
+	BOOKING_TYPE,
+	ADDITIONAL_END_TIME,
+	COST_PER_HOUR,
+} = require('./constants');
+
+module.exports = class RaceTrack {
+	#bookingData = [];
+
+	#checkForInvalidTimeSlot(timeSlot, bookingType) {
+		const endTime = (bookingType === BOOKING_TYPE.BOOKING ? BOOKING_END_TIME : ADDITIONAL_END_TIME);
+		return ((timeSlot < BOOKING_START_TIME) || (timeSlot > endTime));
+	}
+
+	getTotalHours(startTimeString, endTimeString) {
+		const [startTimeHours, startTimeMins] = startTimeString.split(':');
+		const [endTimeHours, endTimeMins] = endTimeString.split(':');
+
+		const differenceInMins = (endTimeHours * 60 + (+endTimeMins))
+        - (startTimeHours * 60 + (+startTimeMins));
+
+		const totalHours = Math.floor(differenceInMins / 60)
+        + (((differenceInMins / 60) - Math.floor(differenceInMins / 60)) > 0 ? 1 : 0);
+
+		return totalHours;
+	}
+
+	addTime(timeString, timeToAdd) {
+		const [hour, minutes] = timeString.split(':');
+		const mins = (hour * 60) + (+minutes) + (+timeToAdd);
+
+		const hoursString = (mins % (24 * 60) / 60 | 0) < 10 ? '0' : `${mins % (24 * 60) / 60 | 0}`;
+		const minsString = (mins % 60) < 10 ? '0' : `${mins % 60}`;
+
+		return (`${hoursString}:${minsString}`);
+	}
+
+	vacancyCheck(vehicleType, entryTime, exitTime) {
+		let countOfVehiclesVip = 0;
+		let countOfVehiclesRegular = 0;
+
+		if (!this.#bookingData.length) {
+			return {
+				regularVehiclesCount: MAX_VEHICLES_PER_TRACK.REGULAR[vehicleType],
+				vipVehiclesCount: MAX_VEHICLES_PER_TRACK.VIP[vehicleType],
+			};
+		}
+
+		this.#bookingData.forEach((booking) => {
+			if (booking.vehicleType !== vehicleType) {
+				return;
+			}
+
+			if (!(
+				((booking.entryTime < exitTime) && (booking.exitTime >= exitTime))
+                || ((booking.entryTime <= entryTime) && (booking.exitTime > entryTime))
+			)) {
+				return;
+			}
+
+			if (booking.trackType === TRACK_TYPE.REGULAR) {
+				countOfVehiclesRegular += 1;
+			} else {
+				countOfVehiclesVip += 1;
+			}
+		});
+
+		return {
+			regularVehiclesCount: MAX_VEHICLES_PER_TRACK.REGULAR[vehicleType] - countOfVehiclesRegular,
+			vipVehiclesCount: MAX_VEHICLES_PER_TRACK.VIP[vehicleType] - countOfVehiclesVip,
+		};
+	}
+
+	book(vehicleType, vehicleNo, entryTime) {
+		if (!(vehicleNo && entryTime && vehicleType)) {
+			console.log('MISSING_REQUIRED _ARGUMENTS');
+			return;
+		}
+
+		if (!TYPE_OF_VEHICLE[vehicleType]) {
+			console.log('INVALID_VEHICLE_TYPE');
+			return;
+		}
+
+		const isInvalid = this.#checkForInvalidTimeSlot(entryTime, BOOKING_TYPE.BOOKING);
+
+		if (isInvalid) {
+			console.log('INVALID_ENTRY_TIME');
+			return;
+		}
+
+		const exitTime = this.addTime(entryTime, 3 * 60);
+
+		const trackVacancy = this.vacancyCheck(vehicleType, entryTime, exitTime);
+
+		if (trackVacancy.regularVehiclesCount < 1 && ((trackVacancy.vipVehiclesCount < 1) || (vehicleType === 'BIKE'))) {
+			console.log('RACE_TRACK_FULL');
+			return;
+		}
+
+		const booking = {
+			vehicleType,
+			entryTime,
+			vehicleNo,
+			trackType: trackVacancy.regularVehiclesCount >= 1 ? TRACK_TYPE.REGULAR : TRACK_TYPE.VIP,
+			bookingType: BOOKING_TYPE.BOOKING,
+			exitTime,
+		};
+
+		this.#bookingData.push(booking);
+
+		console.log('SUCCESS');
+	}
+
+	additional(vehicleNo, newExitTime) {
+		if (!(vehicleNo && newExitTime)) {
+			console.log('MISSING REQUIRED ARGUMENTS');
+			return;
+		}
+
+		const isInvalid = this.#checkForInvalidTimeSlot(newExitTime, BOOKING_TYPE.ADDITIONAL);
+
+		if (isInvalid) {
+			console.log('INVALID_EXIT_TIME');
+			return;
+		}
+
+		const vehicleData = this.#bookingData.find((booking) => (
+			(booking.vehicleNo === vehicleNo && booking.bookingType === BOOKING_TYPE.BOOKING)
+		));
+
+		if (!vehicleData) {
+			console.log('NO_VEHICLE_EXISTS');
+			return;
+		}
+
+		const {
+			vehicleType,
+			exitTime,
+		} = vehicleData;
+
+		const trackVacancy = this.vacancyCheck(vehicleType, exitTime, newExitTime);
+
+		if (trackVacancy.regularVehiclesCount < 1 && ((trackVacancy.vipVehiclesCount < 1) || (vehicleType === 'BIKE'))) {
+			console.log('RACE_TRACK_FULL');
+			return;
+		}
+
+		const bookingAddition = {
+			entryTime: exitTime,
+			exitTime: newExitTime,
+			vehicleType,
+			vehicleNo,
+			trackType: trackVacancy.regularVehiclesCount >= 1 ? TRACK_TYPE.REGULAR : TRACK_TYPE.VIP,
+			bookingType: BOOKING_TYPE.ADDITIONAL,
+		};
+
+		this.#bookingData.push(bookingAddition);
+
+		console.log('SUCCESS');
+	}
+
+	revenue() {
+		let vipTrackIncome = 0;
+		let regularTrackIncome = 0;
+
+		this.#bookingData.forEach((booking) => {
+			// for booking period
+			if (booking.bookingType === BOOKING_TYPE.BOOKING) {
+				if (booking.trackType === TRACK_TYPE.VIP) {
+					vipTrackIncome += (COST_PER_HOUR.VIP[booking.vehicleType] * 3);
+				} else {
+					regularTrackIncome += (COST_PER_HOUR.REGULAR[booking.vehicleType] * 3);
+				}
+				return;
+			}
+
+			// for additional booking period
+
+			if (this.addTime(booking.entryTime, 15) >= booking.exitTime) return;
+
+			const totalHours = this.getTotalHours(booking.entryTime, booking.exitTime);
+
+			if (booking.trackType === TRACK_TYPE.VIP) {
+				vipTrackIncome += (50 * totalHours);
+			} else {
+				regularTrackIncome += (50 * totalHours);
+			}
+		});
+
+		console.log(`${regularTrackIncome} ${vipTrackIncome}`);
+	}
+};
